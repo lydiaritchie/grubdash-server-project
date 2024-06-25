f97799e048e6e5e6a74336adc75fc76c58b2ea43
Lydia Ritchie <lydia.g.ritchie@gmail.com>
2024-06-25 16:17:35 +0700

    Updated knexfile.js with config settings

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,16 @@
 // Update with your config settings.
+const path = require('path');
+require("dotenv").config();
+const {DATABASE_URL} = process.env;
 
+console.log(DATABASE_URL);
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: DATABASE_URL,
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
